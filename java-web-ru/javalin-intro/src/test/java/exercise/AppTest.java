d69417cea5a37923d33230b777c86ea5864fea16
@hexlet/cli <support@hexlet.io>
2025-02-23 21:23:31 +0300

    submit java-web-ru/javalin-intro

---
+++ java-web-ru/javalin-intro/src/test/java/exercise/AppTest.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class AppTest {
+
+    private static Javalin app;
+    private static String baseUrl;
+
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start(0);
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @Test
+    void testRoot() {
+        HttpResponse<String> response = Unirest.get(baseUrl + "/welcome").asString();
+        String content = response.getBody();
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(content).isEqualTo("Welcome to Hexlet!");
+    }
+}
