107b513b11ed0fde6e0e4454cadfc55ef41b2af3
@hexlet/cli <support@hexlet.io>
2025-03-01 22:12:37 +0300

    submit java-web-ru/layout

---
+++ java-web-ru/layout/src/main/java/exercise/dto/users/UsersPage.java
@@ -0,0 +1,13 @@
+package exercise.dto.users;
+
+import exercise.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class UsersPage {
+    private List<User> users;
+}
---
+++ java-web-ru/layout/src/test/java/exercise/AppTest.java
@@ -0,0 +1,68 @@
+package exercise;
+
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AppTest {
+
+    private static Javalin app;
+
+    @BeforeEach
+    public void setUp() {
+        app = App.getApp();
+    }
+
+    @Test
+    void testMainPage() throws Exception {
+
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/");
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string()).contains("/", "/users");
+        });
+    }
+
+    @Test
+    void testUsersPage() throws Exception {
+
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/users");
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string())
+                    .contains("Wehner", "Casper", "Ward")
+                    .contains("/", "/users");
+        });
+    }
+
+    @Test
+    void testUserPage1() throws Exception {
+
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/users/1");
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string())
+                    .contains("/", "/users")
+                    .contains("Pearl")
+                    .contains("Schultz")
+                    .doesNotContain("Wehner");
+        });
+    }
+
+    @Test
+    void testUserPage2() throws Exception {
+
+        JavalinTest.test(app, (server, client) -> {
+            var response = client.get("/users/5");
+            assertThat(response.code()).isEqualTo(200);
+            assertThat(response.body().string())
+                    .contains("/", "/users")
+                    .contains("Minerva")
+                    .contains("Altenwerth")
+                    .doesNotContain("Roob");
+        });
+    }
+}
