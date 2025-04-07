7590a7501d1d135b4e287666d1648ca560c1c5d9
@hexlet/cli <support@hexlet.io>
2025-04-07 15:20:49 +0300

    submit java-web-ru/mvc

---
+++ java-web-ru/mvc/src/main/java/exercise/controller/PostsController.java
@@ -0,0 +1,109 @@
+package exercise.controller;
+
+import exercise.dto.posts.BuildPostPage;
+import exercise.dto.posts.EditPostPage;
+import exercise.dto.posts.PostPage;
+import exercise.dto.posts.PostsPage;
+import exercise.model.Post;
+import exercise.repository.PostRepository;
+import exercise.util.NamedRoutes;
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.validation.ValidationException;
+
+import java.util.HashMap;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+public class PostsController {
+
+    public static void build(Context ctx) {
+        var page = new BuildPostPage();
+        ctx.render("posts/build.jte", model("page", page));
+    }
+
+    public static void create(Context ctx) {
+        try {
+            var name = ctx.formParamAsClass("name", String.class)
+                    .check(value -> value.length() >= 2, "Название не должно быть короче двух символов")
+                    .get();
+
+            var body = ctx.formParamAsClass("body", String.class)
+                    .check(value -> value.length() >= 10, "Пост должен быть не короче 10 символов")
+                    .get();
+
+            var post = new Post(name, body);
+            PostRepository.save(post);
+            ctx.redirect(NamedRoutes.postsPath());
+
+        } catch (ValidationException e) {
+            var name = ctx.formParam("name");
+            var body = ctx.formParam("body");
+            var page = new BuildPostPage(name, body, e.getErrors());
+            ctx.render("posts/build.jte", model("page", page)).status(422);
+        }
+    }
+
+    public static void index(Context ctx) {
+        var posts = PostRepository.getEntities();
+        var postPage = new PostsPage(posts);
+        ctx.render("posts/index.jte", model("page", postPage));
+    }
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var post = PostRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Post not found"));
+
+        var page = new PostPage(post);
+        ctx.render("posts/show.jte", model("page", page));
+    }
+
+    // BEGIN
+    public static void edit(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var post = PostRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Post not found"));
+
+        var page = new EditPostPage(post.getId(),
+                post.getName(), post.getBody(),
+                new HashMap<>());
+
+        ctx.render("posts/edit.jte", model("page", page));
+    }
+
+    public static void update(Context ctx) {
+        try {
+            var id = ctx.pathParamAsClass("id", Long.class).get();
+            var post = PostRepository.find(id)
+                    .orElseThrow(() -> new NotFoundResponse("Post not found"));
+
+            var name = ctx.formParamAsClass("name", String.class)
+                    .check(val -> val.length() >= 2, "Название не должно быть короче двух символов")
+                    .get();
+            var body = ctx.formParamAsClass("body", String.class)
+                    .check(val -> val.length() >= 10, "Пост должен быть не короче 10 символов")
+                    .get();
+
+            if (post.getName().equals(name)
+                    && post.getBody().equals(body)) {
+                System.out.println("The name or body has not changed!");
+                ctx.redirect("/posts");
+                return;
+            }
+
+            post.setName(name);
+            post.setBody(body);
+            PostRepository.save(post);
+
+            ctx.redirect("/posts");
+        } catch (ValidationException e) {
+            var id = ctx.pathParamAsClass("id", Long.class).get();
+            var name = ctx.formParam("name");
+            var body = ctx.formParam("body");
+            var page = new EditPostPage(id, name, body, e.getErrors());
+            ctx.render("posts/edit.jte", model("page", page)).status(422);
+        }
+    }
+    // END
+}
